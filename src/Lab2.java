0ad90f4a07ec3b83ba23be41cb712aeceee1dd38
shipchik <vitalik.konukh@gmail.com>
2024-04-07 21:24:49 +0300

    2

--- src/Lab2.java
+++ src/Lab2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.Vector;
 
 
 class Circle{
@@ -160,7 +160,18 @@ class Book{
     }
 }
 
+class MyMath {
+    static int nok(int a, int b) {
+        return a * b / nod(a, b);
+    }
 
+    static int nod(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return nod(b, a % b);
+    }
+}
 class Fraction{
     int numerator;
     int denominator;
@@ -195,18 +206,65 @@ class Fraction{
     }
     void add(Fraction fr){
 
-        int commonDenominator = this.denominator * fr.denominator;
-        int newNumerator = this.numerator * fr.denominator + fr.numerator * this.denominator;
-        int thisGcd = gcd(newNumerator, commonDenominator);
-        System.out.println(thisGcd);
-        System.out.println(newNumerator/thisGcd+"/"+commonDenominator/thisGcd);
+        int cdenominator = MyMath.nok(this.denominator,fr.denominator);
+        int cNumerator = this.numerator*(cdenominator/ this.denominator) + fr.numerator * (cdenominator/fr.denominator);
+        int ceil = cNumerator/cdenominator;
+        cNumerator -= ceil*cdenominator;
+        System.out.println(ceil +" "+ cNumerator+ "/"+cdenominator);
+    }
+    void add_to_frac(Fraction fr){
+
+        int cdenominator = MyMath.nok(this.denominator,fr.denominator);
+        int cNumerator = this.numerator*(cdenominator/ this.denominator) + fr.numerator * (cdenominator/fr.denominator);
+
+        this.numerator = cNumerator;
+        this.denominator=cdenominator;
+
     }
     void remove(Fraction fr){
         int commonDenominator = this.denominator * fr.denominator;
         int newNumerator = this.numerator * fr.denominator - fr.numerator * this.denominator;
-        int thisGcd = gcd(newNumerator, commonDenominator);
-        System.out.println(newNumerator/thisGcd+"/"+commonDenominator/thisGcd);
+        int thisGcd = MyMath.nod(newNumerator, commonDenominator);
+        int ceil = (newNumerator/thisGcd)/(commonDenominator/thisGcd);
+        newNumerator -= ceil*commonDenominator/thisGcd;
+        System.out.println(ceil+" "+newNumerator+"/"+commonDenominator/thisGcd);
 
+    }
+
+    void multiple(Fraction fr){
+        int commondenominator = this.denominator * fr.denominator;
+        int newnumerator = this.numerator * fr.numerator;
+        int ceil = newnumerator/commondenominator;
+        newnumerator -= ceil*commondenominator;
+        System.out.println(ceil +" "+ newnumerator+ "/"+commondenominator);
+    }
+
+    void devide(Fraction fr){
+        int commondenominator = this.denominator * fr.numerator;
+        int newnumerator = this.numerator * fr.denominator;
+        int ceil = newnumerator/commondenominator;
+        newnumerator -= ceil*commondenominator;
+        System.out.println(ceil +" "+ newnumerator+ "/"+commondenominator);
+    }
+
+
+
+}
+
+
+class MyDouble {
+    private double value;
+
+    public MyDouble(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return this.value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
     }
 }
 
@@ -227,6 +285,53 @@ public class Lab2 {
     static void task4(){
         Fraction fraction1 = new Fraction(11, 5);
         Fraction fraction2 = new Fraction(23, 4);
-        fraction1.add(fraction2);
+        fraction1.multiple(fraction2);
+
+        Vector<Fraction> frac = new Vector<Fraction>(10,1);
+        for(int i=0;i< 10;i++){
+            frac.add(new Fraction((int)Math.random()*6,(int)Math.random()*6));
+        }
+        for(int i=2;i<frac.size();i+=2){
+            frac.elementAt(i).add_to_frac(frac.elementAt(i+1));
+        }
     }
+
+    static void task5(){
+        Double d1 = Double.valueOf(10.6);
+        Double d2 = Double.valueOf("20.5");
+
+
+        double d3 = Double.parseDouble("30.5");
+
+
+        byte b = d1.byteValue();
+        short s = d1.shortValue();
+        int i = d1.intValue();
+        long l = d1.longValue();
+        float f = d1.floatValue();
+
+
+        System.out.println("Значение d1: " + d1);
+
+
+        String dStr = Double.toString(3.14);
+        System.out.println("Строка dStr: " + dStr);
+    }
+
+    static void task6(){
+        String str = "I like Java!!!";
+        System.out.println("Последний символ: " + str.charAt(str.length() - 1));
+        System.out.println("Заканчивается на '!!!': " + str.endsWith("!!!"));
+        System.out.println("Начинается с 'I like': " + str.startsWith("I like"));
+        System.out.println("Содержит 'Java': " + str.contains("Java"));
+        System.out.println("Позиция 'Java': " + str.indexOf("Java"));
+        System.out.println("Замена 'a' на 'o': " + str.replace('a', 'o'));
+        System.out.println("Верхний регистр: " + str.toUpperCase());
+        System.out.println("Нижний регистр: " + str.toLowerCase());
+        int start = str.indexOf("Java");
+        if (start != -1) {
+            System.out.println("Подстрока 'Java': " + str.substring(start, start + "Java".length()));
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -252,7 +252,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-            Lab2.task4();
+            Lab2.task6();
 
 
     }
